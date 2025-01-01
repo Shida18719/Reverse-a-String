9b797211f7b0db1752bff2dd7dbf1a1ec63f649f
Rashidat <rawsheedah@yahoo.com>
2025-01-01 15:29:01 +0000

    Reverse a string with Scanner and a for loop

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         String str;
-        // Reverse string using Scanner to take input and a For Loop for iteration
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string character here : ");
-        str = sc.nextLine();
+        try (
+            // Reverse string using Scanner to take input and a For Loop for iteration
+        Scanner userInput = new Scanner(System.in)) {
+            System.out.print("Enter a string character here : ");
+            str = userInput.nextLine();
+        }
         System.out.println("Reverse a String '" + str +"' is :");
         for(int i = str.length(); i > 0; --i) {
             System.out.print(str.charAt(i-1));
